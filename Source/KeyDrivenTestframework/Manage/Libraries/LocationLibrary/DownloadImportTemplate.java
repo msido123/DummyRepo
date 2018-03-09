72ce693a2697fe901e32d355145123700fa82b6f
Phathuxolo Juqu <PhathuxoloJ@mixtelematics.com>
2018-03-09 14:20:26 +0200

    Created a file for running the test

--- Source/KeyDrivenTestframework/Manage/Libraries/LocationLibrary/DownloadImportTemplate.java
+++ Source/KeyDrivenTestframework/Manage/Libraries/LocationLibrary/DownloadImportTemplate.java
@@ -5,10 +5,27 @@
  */
 package KeyDrivenTestframework.Manage.Libraries.LocationLibrary;
 
+import KeywordDrivenTestFramework.Core.BaseClass;
+import KeywordDrivenTestFramework.Entities.Enums;
+import KeywordDrivenTestFramework.Entities.TestEntity;
+import KeywordDrivenTestFramework.Entities.TestResult;
+
 /**
  *
  * @author PhathuxoloJ
  */
-public class DownloadImportTemplate {
+public class DownloadImportTemplate extends BaseClass{
     
+    int counter = 1;
+    TestEntity testData;
+    String errorMessage;
+    
+    public DownloadImportTemplate(TestEntity testdata){
+        this.testData = testData;
+    }
+    
+    public TestResult runTest() throws InterruptedException{
+        this.setStartTime();
+        return new TestResult(testData,Enums.ResultStatus.PASS,"Import template downloaded succesfully",this.getTotalExecutionTime());
+    }
 }
