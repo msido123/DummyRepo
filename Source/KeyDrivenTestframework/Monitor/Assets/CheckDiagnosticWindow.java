65de7b9e6c7a7a02009f13d76211074f27f7f3a9
Phathuxolo Juqu <PhathuxoloJ@mixtelematics.com>
2018-03-14 10:48:05 +0200

    Completed check diagnostic window

--- Source/KeyDrivenTestframework/Monitor/Assets/CheckDiagnosticWindow.java
+++ Source/KeyDrivenTestframework/Monitor/Assets/CheckDiagnosticWindow.java
@@ -33,47 +33,102 @@ public class CheckDiagnosticWindow extends BaseClass{
             narrator.failedMessage("Failed to click on Monitor Tab - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click on Monitor Tab",this.getTotalExecutionTime());
         }
-        
+        pause(1000);
         if(!methods.navigateToAssetsTab()){
             SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click on Assets Tab", true);
             counter++;
             narrator.failedMessage("Failed to click on Assets Tab - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click on Assets Tab",this.getTotalExecutionTime());
         }
-        
+        pause(1000);
         if(!methods.clickFilter()){
             SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click on Filter", true);
             counter++;
             narrator.failedMessage("Failed to click on Filter - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click on Filter",this.getTotalExecutionTime());
         }
-        
+        pause(1000);
         if(!methods.enterTextToFilter("Mix Automation Live Unit")){
             SeleniumDriverInstance.takeScreenShot(counter + " - Failed to enter text into filter", true);
             counter++;
             narrator.failedMessage("Failed to enter text into filter - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to enter text into filter",this.getTotalExecutionTime());
         }
-        
+        pause(1000);
         if(!methods.clickSearchButton()){
             SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click search button", true);
             counter++;
             narrator.failedMessage("Failed to click search button - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click search button",this.getTotalExecutionTime());
         }
-        
+        pause(1000);
         if(!methods.clickDropDownActionMenu()){
             SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click drop down action menu ", true);
             counter++;
             narrator.failedMessage("Failed to click drop down action menu  - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click drop down action menu ",this.getTotalExecutionTime());
         }
-        
+        pause(1000);
         if(!methods.clickDiagnostic()){
             SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click Diagnostic on drop down action menu ", true);
             counter++;
             narrator.failedMessage("Failed to click Diagnostic on drop down action menu  - "+errorMessage);
             return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click Diagnostic on drop down action menu ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.clickGeneralStatusInfo()){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click General status information bar ", true);
+            counter++;
+            narrator.failedMessage("Failed to click General status information bar  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click General status information bar ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.verifyLabel("//strong[text()='Driver']","Driver")){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to verify labels ", true);
+            counter++;
+            narrator.failedMessage("Failed to verify labels  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to verify labels ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.verifyLabel("//strong[text()='GPRS context (restricted)']","GPRS context (restricted)")){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to verify labels ", true);
+            counter++;
+            narrator.failedMessage("Failed to verify labels  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to verify labels ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.verifyData("MiX DriverJM","+CGDCONT=1,\"IP\",\"internet\"")){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to verify data ", true);
+            counter++;
+            narrator.failedMessage("Failed to verify data  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to verify data ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.clickLatestPosition()){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to click latest position ", true);
+            counter++;
+            narrator.failedMessage("Failed to click latest position  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to click latest position ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.scrollDown()){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to scroll down ", true);
+            counter++;
+            narrator.failedMessage("Failed to scroll down  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to scroll down ",this.getTotalExecutionTime());
+        }
+        pause(1000);
+        if(!methods.verifyLabel("(//TD)[225]","HDOP")){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to verify labels ", true);
+            counter++;
+            narrator.failedMessage("Failed to verify labels  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to verify labels ",this.getTotalExecutionTime());
+        }
+        if(!methods.closePopUp()){
+            SeleniumDriverInstance.takeScreenShot(counter + " - Failed to close pop up ", true);
+            counter++;
+            narrator.failedMessage("Failed to close pop up  - "+errorMessage);
+            return new TestResult(testData,Enums.ResultStatus.FAIL,"Failed to close pop up ",this.getTotalExecutionTime());
         }
         return new TestResult(testData,Enums.ResultStatus.PASS,"Diagnostic window loads with correct information",this.getTotalExecutionTime());
     }
--- Source/KeyDrivenTestframework/Monitor/Assets/DiagnosticWindowMethods.java
+++ Source/KeyDrivenTestframework/Monitor/Assets/DiagnosticWindowMethods.java
@@ -16,7 +16,7 @@ public class DiagnosticWindowMethods extends CheckDiagnosticWindow{
     public DiagnosticWindowMethods(TestEntity testData) {
         super(testData);
     }
-    
+    String parentHandle ;
     public boolean navigateToMonitorTab(){
         if(!SeleniumDriverInstance.waitForElementByXpath("(//A[@href='#'])[1]")){
             errorMessage = "Failed to click Monitor tab";
@@ -24,6 +24,7 @@ public class DiagnosticWindowMethods extends CheckDiagnosticWindow{
         }
         SeleniumDriverInstance.clickElementByXpath("(//A[@href='#'])[1]");
         SeleniumDriverInstance.takeScreenShot("Clicked Monitor tab succesfully", false);
+        parentHandle = SeleniumDriverInstance.Driver.getWindowHandle();
         counter++;
         return true;          
     }
@@ -94,7 +95,100 @@ public class DiagnosticWindowMethods extends CheckDiagnosticWindow{
         return true;
     }
     
+    public void maximizePopUpWindow(){
+        SeleniumDriverInstance.SwitchToNewWindow();
+        pause(1000);
+        SeleniumDriverInstance.Driver.manage().window().maximize();
+    }
     
+    public boolean clickGeneralStatusInfo(){
+        maximizePopUpWindow();
+        if(!SeleniumDriverInstance.waitForElementByXpath("//span[@class='ng-binding'][text()='General status information']")){
+            errorMessage = "Failed to click on general status information bar";
+            return false;
+        }
+        SeleniumDriverInstance.clickElementByXpath("//span[@class='ng-binding'][text()='General status information']");
+        SeleniumDriverInstance.takeScreenShot("Clicked general status information bar successfully ",false);
+        counter++;
+        return true;
+    }
     
+    public boolean verifyData(String Driver,String GPRS){
+        if(!SeleniumDriverInstance.waitForElementByXpath("//td[@class='ng-binding'][text()='MiX DriverJM']") && !SeleniumDriverInstance.waitForElementByXpath("//td[@class='ng-binding'][text()='+CGDCONT=1,\"IP\",\"internet\"']") ){
+            errorMessage = "Failed to extract data from the grid";
+            return false;
+        }
+        String gridDriver = SeleniumDriverInstance.retrieveTextByXpath("//td[@class='ng-binding'][text()='MiX DriverJM']");
+        String gridGPRS = SeleniumDriverInstance.retrieveTextByXpath("//td[@class='ng-binding'][text()='+CGDCONT=1,\"IP\",\"internet\"']");
+        if(!(Driver.contains(gridDriver) && GPRS.contains(gridGPRS))){
+            errorMessage = "Failed to match entered data with data in the grid";
+            testData.extractParameter("Driver : ",gridDriver , "FAIL");
+            testData.extractParameter("GPRS context(restricted) : ",gridGPRS , "FAIL");
+            SeleniumDriverInstance.takeScreenShot("Failed to match entered data with data in the grid",false);
+            counter++;
+            return false;
+        }
+        testData.extractParameter("Driver : ",gridDriver,"PASS");
+        testData.extractParameter("GPRS context(restricted) : ",gridGPRS,"PASS");
+        SeleniumDriverInstance.takeScreenShot("Entered data matches the grid data",false);
+        counter++;
+        return true;
+    }
+    
+    public boolean clickLatestPosition(){
+        if(!SeleniumDriverInstance.waitForElementByXpath("//span[@class='ng-binding'][text()='Latest position information']")){
+            errorMessage = "Failed to click on Latest position information bar";
+            return false;
+        }
+        SeleniumDriverInstance.clickElementByXpath("//span[@class='ng-binding'][text()='Latest position information']");
+        SeleniumDriverInstance.takeScreenShot("Clicked latest position information bar successfully ",false);
+        counter++;
+        return true;
+    }
+    int i = 0;
+    public boolean verifyLabel(String Xpath,String labelToCheck){
+        if(!SeleniumDriverInstance.waitForElementByXpath(Xpath)){
+            errorMessage = "No label found";
+            System.out.println(errorMessage);
+            return false;
+        }
+        String extractedLabel = SeleniumDriverInstance.retrieveTextByXpath(Xpath);
+        if(!extractedLabel.contains(labelToCheck)){
+            errorMessage = "Failed to verify labels";
+            i++;
+            testData.extractParameter("Label "+ i, extractedLabel, "FAIL");
+            SeleniumDriverInstance.takeScreenShot("Failed to verify labels", false);
+            counter++;
+            return false;
+        }
+        i++;
+        testData.extractParameter("Label "+ i, extractedLabel, "PASS");
+        SeleniumDriverInstance.takeScreenShot("Labels match 100%", false);
+        counter++;
+        return true;
+    }
+    
+    public boolean scrollDown(){
+        if(!SeleniumDriverInstance.waitForElementByXpath("(//TD)[225]")){
+            errorMessage = "could not scroll to element";
+            return false;
+        }
+        SeleniumDriverInstance.scrollToElement("(//TD)[225]");
+        SeleniumDriverInstance.takeScreenShot("Mouse scrolled to element successfully", false);
+        counter++;
+        return true;
+    }
+    
+    public boolean closePopUp(){
+        if(!SeleniumDriverInstance.isDriverRunning()){
+            errorMessage = "Failed to close pop up window";
+            return false;
+        }
+        SeleniumDriverInstance.SwitchToOriginalWindowNoClose(parentHandle);
+        pause(500);
+        SeleniumDriverInstance.takeScreenShot("Closed pop up window succesfully",false);
+        counter++;
+        return true;
+    }
     
 }
